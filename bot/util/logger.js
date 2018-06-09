723f83548eff14da37cce88a64c1ba6c783b6c89
Oskar Lang <oskar.risto.lang@gmail.com>
2018-06-09 15:21:24 +0200

    Restructured Code
    Restructured logger code as there were missing functionalities

--- bot/util/logger.js
+++ bot/util/logger.js
@@ -10,6 +10,7 @@ exports.error = (content) => {
     content.split('\n').forEach(s => {
         console.log(`[${time}] `.white + ` ${'[ ERROR ]'} `.red + ` ${s}`);
         write_error(`[${time}] ` + ` ${'[ ERROR ]'} ` + ` ${s}`);
+        write_full(`[${time}] ` + ` ${'[ ERROR ]'} ` + ` ${s}`);
     });
 }
 
@@ -23,6 +24,7 @@ exports.info = (content) => {
     content.split('\n').forEach(s => {
         console.log(`[${time}] `.white + ` ${'[ INFO ]'} `.green + ` ${s}`);
         write_info(`[${time}] ` + ` ${'[ INFO ]'} ` + ` ${s}`);
+        write_full(`[${time}] ` + ` ${'[ INFO ]'} ` + ` ${s}`);
     });
 }
 
@@ -36,19 +38,20 @@ exports.command = (content) => {
     content.split('\n').forEach(s => {
         console.log(`[${time}] `.white + ` ${'[ COMMAND ]'} `.yellow + ` ${s}`);
         write_command(`[${time}] ` + ` ${'[ COMMAND ]'} ` + ` ${s}`);
+        write_full(`[${time}] ` + ` ${'[ COMMAND ]'} ` + ` ${s}`);
     });
 }
 
 function write_error(text) {
     var time = new Date().toISOString().split("T")[0];
 
-    fs.stat(`logs/Groovy_Error_${time}.txt`, function(err, stat) {
+    fs.stat(`logs/Error_${time}.txt`, function(err, stat) {
         if(err == null) {
-            fs.appendFile(`logs/Groovy_Error_${time}.txt`, `${text}\n`, (err) => {
+            fs.appendFile(`logs/Error_${time}.txt`, `${text}\n`, (err) => {
                 if (err) console.log(err);
             }); 
         } else if(err.code == 'ENOENT') {
-            fs.writeFile(`logs/Groovy_Error_${time}.txt`, `${text}\n`, (err) => {
+            fs.writeFile(`logs/Error_${time}.txt`, `${text}\n`, (err) => {
                 if (err) console.log(err);
             });
         } else {
@@ -60,13 +63,13 @@ function write_error(text) {
 function write_info(text) {
     var time = new Date().toISOString().split("T")[0];
 
-    fs.stat(`logs/Groovy_Info_${time}.txt`, function(err, stat) {
+    fs.stat(`logs/Info_${time}.txt`, function(err, stat) {
         if(err == null) {
-            fs.appendFile(`logs/Groovy_Info_${time}.txt`, `${text}\n`, (err) => {
+            fs.appendFile(`logs/Info_${time}.txt`, `${text}\n`, (err) => {
                 if (err) console.log(err);
             }); 
         } else if(err.code == 'ENOENT') {
-            fs.writeFile(`logs/Groovy_Info_${time}.txt`, `${text}\n`, (err) => {
+            fs.writeFile(`logs/Info_${time}.txt`, `${text}\n`, (err) => {
                 if (err) console.log(err);
             });
         } else {
@@ -78,17 +81,46 @@ function write_info(text) {
 function write_command(text) {
     var time = new Date().toISOString().split("T")[0];
 
-    fs.stat(`logs/Groovy_Commands_${time}.txt`, function(err, stat) {
+    fs.stat(`logs/Commands_${time}.txt`, function(err, stat) {
         if(err == null) {
-            fs.appendFile(`logs/Groovy_Commands_${time}.txt`, `${text}\n`, (err) => {
+            fs.appendFile(`logs/Commands_${time}.txt`, `${text}\n`, (err) => {
                 if (err) console.log(err);
             }); 
         } else if(err.code == 'ENOENT') {
-            fs.writeFile(`logs/Groovy_Commands_${time}.txt`, `${text}\n`, (err) => {
+            fs.writeFile(`logs/Commands_${time}.txt`, `${text}\n`, (err) => {
                 if (err) console.log(err);
             });
         } else {
             console.log('Some other error: ', err.code);
         }
     });
+}
+
+function write_full(text) {
+    var time = new Date().toISOString().split("T")[0];
+
+    fs.stat(`logs/Groovy_${time}.txt`, function(err, stat) {
+        if(err == null) {
+            fs.appendFile(`logs/Groovy_${time}.txt`, `${text}\n`, (err) => {
+                if (err) console.log(err);
+            }); 
+        } else if(err.code == 'ENOENT') {
+            fs.writeFile(`logs/Groovy_${time}.txt`, `${text}\n`, (err) => {
+                if (err) console.log(err);
+            });
+        } else {
+            console.log('Some other error: ', err.code);
+        }
+    });
+}
+
+exports.write_command = (text) => write_command();
+exports.log_command = (text) => {
+    var first = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '').split(" ")[1];
+    var upgrade = first.split(":");
+
+    upgrade[0] = parseInt(upgrade[0]) + 2;
+    var time = upgrade.join(":");
+
+    console.log(`[${time}] `.white + ` ${'[ COMMAND ]'} `.yellow + ` ${text}`);
 }
