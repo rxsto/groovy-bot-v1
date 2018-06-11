81e7658f07d6785f1dfcf1be87ca97824c4081a3
Oskar Lang <oskar.risto.lang@gmail.com>
2018-06-11 18:16:59 +0200

    Removed useless code

--- bot/util/logger.js
+++ bot/util/logger.js
@@ -28,20 +28,6 @@ exports.info = (content) => {
     });
 }
 
-exports.command = (content) => {
-    var first = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '').split(" ")[1];
-    var upgrade = first.split(":");
-
-    upgrade[0] = parseInt(upgrade[0]) + 2;
-    var time = upgrade.join(":");
-
-    content.split('\n').forEach(s => {
-        console.log(`[${time}] `.white + ` ${'[ COMMAND ]'} `.yellow + ` ${s}`);
-        write_command(`[${time}] ` + ` ${'[ COMMAND ]'} ` + ` ${s}`);
-        write_full(`[${time}] ` + ` ${'[ COMMAND ]'} ` + ` ${s}`);
-    });
-}
-
 function write_error(text) {
     var time = new Date().toISOString().split("T")[0];
 
@@ -78,24 +64,6 @@ function write_info(text) {
     });
 }
 
-function write_command(text) {
-    var time = new Date().toISOString().split("T")[0];
-
-    fs.stat(`logs/Commands_${time}.txt`, function(err, stat) {
-        if(err == null) {
-            fs.appendFile(`logs/Commands_${time}.txt`, `${text}\n`, (err) => {
-                if (err) console.log(err);
-            }); 
-        } else if(err.code == 'ENOENT') {
-            fs.writeFile(`logs/Commands_${time}.txt`, `${text}\n`, (err) => {
-                if (err) console.log(err);
-            });
-        } else {
-            console.log('Some other error: ', err.code);
-        }
-    });
-}
-
 function write_full(text) {
     var time = new Date().toISOString().split("T")[0];
 
@@ -114,13 +82,13 @@ function write_full(text) {
     });
 }
 
-exports.write_command = (text) => write_command();
-exports.log_command = (text) => {
+exports.write_info = (text) => write_info();
+exports.log_info = (text) => {
     var first = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '').split(" ")[1];
     var upgrade = first.split(":");
 
     upgrade[0] = parseInt(upgrade[0]) + 2;
     var time = upgrade.join(":");
 
-    console.log(`[${time}] `.white + ` ${'[ COMMAND ]'} `.yellow + ` ${text}`);
+    console.log(`[${time}] `.white + ` ${'[ INFO ]'} `.green + ` ${text}`);
 }
