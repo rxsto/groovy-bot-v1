2eb7f41b71b351babd7de6f06bc98cda6e136339
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-05 08:28:17 +0200

    Added more texts

--- utilities/game_animator.py
+++ utilities/game_animator.py
@@ -12,8 +12,19 @@ class GameAnimator:
         games = [
             f'on {len(self.client.guilds)} servers',
             f'playing for {len(self.client.users)} users',
+            f'playing in {len(self.client.voice_clients)} voicechannels',
+            f'currently serving {self.client.shard_count} shards',
+            f'you\'re on shard {self.client.shard_id}'
             f'listen to some f*cking music!',
-            f'how you doin\'?'
+            f'how you doin\'?',
+            f'join my server!',
+            f'I\'m Groovy, you know me?',
+            f'better than Rythm',
+            f'it\'s not a bug, it\'s a feature!',
+            f'check .help',
+            f'patreon.com/rxsto',
+            f'twitter.com/rxsto_official',
+            f'github.com/rxsto'
         ]
         game = Game(f'@Groovy - {random.choice(games)}')
         self.event_loop.create_task(
