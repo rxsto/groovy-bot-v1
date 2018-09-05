cb7e9cfdc924eb7d4c36210dd61fbeaa7edc7dbf
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-05 11:06:47 +0200

    Added shard command

---
+++ cogs/shard.py
@@ -0,0 +1,14 @@
+from discord.ext import commands
+
+
+def setup(bot):
+    bot.add_cog(Shard(bot))
+
+
+class Shard:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def shard(self, ctx):
+        await ctx.send(f':information_source: You\'re on **shard `{ctx.author.guild.shard_id + 1}`**')
