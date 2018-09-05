4b1fc99b063171d69c2e0caebef0f741d287a101
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-05 08:27:40 +0200

    Added support command

---
+++ cogs/support.py
@@ -0,0 +1,14 @@
+from discord.ext import commands
+
+
+def setup(bot):
+    bot.add_cog(Support(bot))
+
+
+class Support:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def support(self, ctx):
+        await ctx.send(':information_source: You can join my support server here: url')
