fed9bf3848332b64c1837e7e5ec7742e26c13065
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-05 08:26:13 +0200

    Added help and automation

--- cogs/help.py
+++ cogs/help.py
@@ -1,6 +1,9 @@
+import discord
+
 from discord.ext import commands
 
-from utilities import texts
+from utilities import cogs
+from utilities import logger
 
 
 def setup(bot):
@@ -11,7 +14,21 @@ def setup(bot):
 class Help:
     def __init__(self, bot):
         self.bot = bot
+        self.title = ':information_source: All commands and features'
+
+        self.msg = ':white_small_square: **command** [alias1, alias2, ...] - `description`\n\n'
+
+        logger.info(f'Started generating help message ...')
+
+        for cog in cogs.cogs:
+            logger.info(f'Adding {cog[0]} ...')
+            aliases = ''
+            if len(cog) is 3:
+                aliases += f' [{cog[2]}]'
+
+            self.msg += f':white_small_square: **{cog[0]}**{aliases} - `{cog[1]}`\n'
 
     @commands.command(aliases=['?'])
     async def help(self, ctx):
-        await ctx.send(texts.help_text)
+        embed = discord.Embed(title=self.title, description=self.msg, color=ctx.guild.me.top_role.color)
+        await ctx.send(embed=embed)
