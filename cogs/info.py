f81e2286a7ad59efae00a093d5b7625e04bd4b86
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-05 08:26:53 +0200

    Changed text

--- cogs/info.py
+++ cogs/info.py
@@ -1,7 +1,5 @@
 from discord.ext import commands
 
-from utilities import texts
-
 
 def setup(bot):
     bot.add_cog(Info(bot))
@@ -13,4 +11,7 @@ class Info:
 
     @commands.command()
     async def info(self, ctx):
-        await ctx.send(texts.info_text)
+        await ctx.send(':information_source: **This bot is currently under beta!** Please report any issue!\n'
+                       'You can get all commands and features by typing **`.help`**, '
+                       'if you want to join my support server, type **`.support`**. '
+                       'You can invite me by typing **`.invite`**')
