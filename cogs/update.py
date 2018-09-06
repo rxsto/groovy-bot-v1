bb7487e62080bd833f09c3550ad4bc066f22f2a0
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-06 19:16:36 +0200

    Added update file

--- cogs/update.py
+++ cogs/update.py
@@ -11,12 +11,17 @@ class Update:
 
     @commands.command()
     async def update(self, ctx):
-        if ctx.author.id is not 254892085000405004 or ctx.author.id is not 264048760580079616:
-            return ctx.send(':no_entry_sign: This command is only executable by the devs!')
+        trusted_users = [254892085000405004, 264048760580079616]
+        if ctx.author.id not in trusted_users:
+            return await ctx.send(':no_entry_sign: This command is only executable by the devs!')
         players = self.bot.lavalink.players
 
-        for player in players:
-            await player.fetch('channel').send(':warning: **Groovy is going to restart soon! '
-                                               'Please be patient! For further information '
-                                               'check Groovy\'s official support server: '
-                                               'https://groovybot.gq/support **')
+        for player_tuple in players:
+            player = player_tuple[1]
+            channel_id = player.fetch('channel')
+            guild = self.bot.get_guild(player_tuple[0])
+            channel = guild.get_channel(channel_id)
+            await channel.send(':warning: **Groovy is going to restart soon! '
+                               'Please be patient! For further information '
+                               'check Groovy\'s official support server: '
+                               'https://groovybot.gq/support **')
