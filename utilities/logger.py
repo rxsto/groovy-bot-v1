111a82292277946db5330e5b3a9943536b07443c
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-06 10:32:04 +0200

    Changed logger

--- utilities/logger.py
+++ utilities/logger.py
@@ -1,7 +1,15 @@
+import datetime
 import os
 import time
+import aiohttp
+import asyncio
+import discord
+
+from discord import Webhook, AsyncWebhookAdapter
 
 from enum import Enum
+
+from utilities.config import Config
 
 
 class LogLevel(Enum):
@@ -13,29 +21,23 @@ class LogLevel(Enum):
 
 
 def debug(message):
-    log(LogLevel.DEBUG, message)
+    asyncio.get_event_loop().create_task(log(LogLevel.DEBUG, message))
 
 
 def info(message):
-    log(LogLevel.INFO, message)
+    asyncio.get_event_loop().create_task(log(LogLevel.INFO, message))
 
 
 def warn(message):
-    log(LogLevel.WARN, message)
+    asyncio.get_event_loop().create_task(log(LogLevel.WARN, message))
 
 
-def error(message, *exception: Exception):
-    log(LogLevel.ERROR, message)
-    if exception is None:
-        return
-    print(exception)
+def error(message, exception: Exception = None):
+    asyncio.get_event_loop().create_task(log(LogLevel.ERROR, message, exception))
 
 
-def critical(message, *exception: Exception):
-    log(LogLevel.CRITICAL, message)
-    if exception is None:
-        return
-    print(exception)
+def critical(message, exception: Exception = None):
+    asyncio.get_event_loop().create_task(log(LogLevel.CRITICAL, message, exception))
 
 
 def init():
@@ -48,9 +50,24 @@ def init():
     file.write(f'[{own_time}] [INFO] Initialized logging file while booting on {init_time}\n')
 
 
-def log(level, message):
+async def log(level, message, exception: Exception = None):
     file = open(f'logs/obstBot_{time.strftime("%d-%m-%Y")}.log', "a")
     own_time = time.strftime("%H:%M:%S")
-    print(f'[{own_time}] [{level.name}] {message}')
-    file.write(f'[{own_time}] [{level.name}] {message}\n')
+    formatted_message = f'[{own_time}] [{level.name}] {message}'
+    print(formatted_message)
+    file.write(formatted_message + '\n')
+    if exception is not None:
+        exception_string = f'{exception.__class__.__name__}: {exception}'
+        print(exception_string)
+        file.write(exception_string + '\n')
+        async with aiohttp.ClientSession() as session:
+            error_hook = Webhook.from_url(Config().get_config()['webhooks']['error'],
+                                          adapter=AsyncWebhookAdapter(session))
+            await error_hook.send(embed=discord.Embed(
+                title=":no_entry_sign: An internal error occurred!",
+                timestamp=datetime.datetime.now(),
+                color=0xf22b2b,
+                description=f'```{exception_string}```'
+            ))
+
     file.close()
