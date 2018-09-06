0e7df7ff62f29f1547a137763d142be3976a4c5d
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-06 19:17:11 +0200

    Added texts

--- utilities/game_animator.py
+++ utilities/game_animator.py
@@ -11,21 +11,31 @@ class GameAnimator:
     def run(self):
         games = [
             f'on {len(self.client.guilds)} servers',
-            f'playing for {len(self.client.users)} users',
-            f'playing in {len(self.client.voice_clients)} voicechannels',
-            f'currently serving {self.client.shard_count} shards',
-            f'listen to some f*cking music!',
-            f'how you doin\'?',
-            f'join my server!',
+            f'for {len(self.client.users)} users',
+            f'in {len(self.client.lavalink.players)} voicechannels',
+            f'on {self.client.shard_count} shards',
+            f'Listen to some f*cking music!',
+            f'How you doin\'?',
+            f'Join my server!',
+            f'Let\'s get the biggest bot on Discord! Muhahahahaa!',
             f'I\'m Groovy, you know me?',
-            f'better than Rythm',
-            f'it\'s not a bug, it\'s a feature!',
-            f'check .help',
+            f'Mention me! Like @Groovy',
+            f'Better than Rythm',
+            f'It\'s not a bug, it\'s a feature!',
+            f'Check g!help',
+            f'Do you like what you hear?',
+            f'Vote for me!',
+            f'2 + 2 = 5!',
+            f'Skibidi papapapa! Skraaa!',
+            f'I am written in Delphi!',
+            f'Composing some lit music!',
+            f'Generating useless hash-codes!',
+            f'groovybot.gq',
             f'patreon.com/rxsto',
             f'twitter.com/rxsto_official',
             f'github.com/rxsto'
         ]
-        game = Game(f'@Groovy - {random.choice(games)}')
+        game = Game(f'{random.choice(games)}')
         self.event_loop.create_task(
             self.client.change_presence(status=Status.online, activity=game))
         threading.Timer(60.0, self.run).start()
