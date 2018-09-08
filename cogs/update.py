99ac956ddd64030b7e71bdd115d79d84adc3cd18
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-08 14:28:44 +0200

    Optimized update system

--- cogs/update.py
+++ cogs/update.py
@@ -1,6 +1,7 @@
 import asyncio
 import time
 
+from discord import Status, Game
 from discord.ext import commands
 
 
@@ -18,9 +19,17 @@ class Update:
         self.bot.set_updating(True)
         if ctx.author.id not in trusted_users:
             return await ctx.send(':no_entry_sign: This command is only executable by the devs!')
+
+        game = Game('Updating!')
+        await self.bot.change_presence(status=Status.online, activity=game)
+
         players = self.bot.lavalink.players
 
-        status_msg = await ctx.send('<a:groovyloading:487681291010179072> Announcing groovy update')
+        status_msg = await ctx.send('<a:groovyloading:487681291010179072> Announcing **Update** - '
+                                    'Please **wait** until the process has **finished**!')
+
+        await self.bot.update_outage_channel(outage='restart')
+
         for player_tuple in players:
             player = player_tuple[1]
             channel_id = player.fetch('channel')
@@ -35,26 +44,32 @@ class Update:
                 return
 
             if player.current is None:
-                return
-            track_list = get_track_list(player.queue)
+                await channel.send(':warning: **Groovy is going to restart soon! Please be patient! '
+                                   'For further information check Groovy\'s official support server:** '
+                                   'https://groovybot.gq/support')
+            else:
+                track_list = get_track_list(player.queue)
 
-            async with self.bot.get_postgre_client().get_pool().acquire() as connection:
-                statement = await connection.prepare(
-                    'INSERT INTO queues (guild_id, current_track, current_position, queue, channel_id, text_channel_id)'
-                    ' VALUES ($1, $2, $3, $4, $5, $6)')
-                await statement.fetchval(player_tuple[0], player.current.uri, player.position, str(track_list),
-                                         int(player.channel_id), channel_id)
-            await player.set_volume(0)
-            await asyncio.sleep(3)
-            player.queue.clear()
-            sound = await self.bot.lavalink.get_tracks('https://cdn.groovybot.gq/sounds/update.mp3')
-            player.add(requester=254892085000405004, track=sound['tracks'][0])
-            player.add(requester=264048760580079616, track=sound['tracks'][0])
-            await player.skip()
-            await player.set_volume(100)
-            await asyncio.sleep(6.06)
-            await player.disconnect()
-        await status_msg.edit(content=':white_check_mark: **Finished!** Groovy is going to shutdown now!')
+                async with self.bot.get_postgre_client().get_pool().acquire() as connection:
+                    statement = await connection.prepare(
+                        'INSERT INTO queues ('
+                        'guild_id, current_track, current_position, queue, channel_id, text_channel_id'
+                        ')'
+                        ' VALUES ($1, $2, $3, $4, $5, $6)')
+                    await statement.fetchval(player_tuple[0], player.current.uri, player.position, str(track_list),
+                                             int(player.channel_id), channel_id)
+                await player.set_volume(0)
+                await asyncio.sleep(3)
+                player.queue.clear()
+                sound = await self.bot.lavalink.get_tracks('https://cdn.groovybot.gq/sounds/update.mp3')
+                player.add(requester=254892085000405004, track=sound['tracks'][0])
+                await player.skip()
+                if player.paused:
+                    await player.set_pause(False)
+                await player.set_volume(100)
+                await asyncio.sleep(8)
+                await player.disconnect()
+        await status_msg.edit(content=':white_check_mark: **Groovy is able to be restarted!**')
         time.sleep(5)
 
 
