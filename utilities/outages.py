09fa453772faa2d942d52208acc4c7c423cccb25
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-08 14:27:27 +0200

    Added outages dict

---
+++ utilities/outages.py
@@ -0,0 +1,9 @@
+outages = {
+    'operational': ':white_check_mark: **No outages as of now, report anything you encounter!**',
+    'restart': ':warning: **Groovy is going to restart soon!**\n\n'
+               'We are going to update Groovy now, he should be back in a few seconds! '
+               '**Please be patient!**',
+    'investigating': ':no_entry_sign: **We are investigating an issue that occurred!**\n\n'
+                     'The bot may be infunctional or inoperational the next time! **Please be patient, '
+                     'and report anything you encounter!**'
+}
