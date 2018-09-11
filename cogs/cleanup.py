72a681c02323ebddf36229500aec7d402427e0d2
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-11 23:32:16 +0200

    Added command cleanup

---
+++ cogs/cleanup.py
@@ -0,0 +1,30 @@
+from discord.ext import commands
+
+
+def setup(bot):
+    bot.add_cog(Cleanup(bot))
+
+
+class Cleanup:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=['clean'])
+    async def cleanup(self, ctx):
+        player = self.bot.lavalink.players.get(ctx.guild.id)
+
+        members = []
+
+        for member in ctx.me.voice.channel.members:
+            members.append(member.id)
+
+        new_queue = []
+
+        for song in player.queue:
+            if song.requester in members:
+                new_queue.append(song)
+
+        player.queue = new_queue
+
+        ctx.send('✅ Successfully cleaned the queue! All tracks from users that are currently in this voicechannel '
+                 'are still in the queue!')
