16cff492f758eaccd486890e4f3bb5aa2cabc8d4
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-16 13:08:23 +0200

    Imports, texts, removed fade

--- cogs/leave.py
+++ cogs/leave.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
-from cogs.music import Music
+
+from utilities import checks
 
 
 def setup(bot):
@@ -11,21 +12,19 @@ class Leave:
         self.bot = bot
 
     @commands.command(aliases=['exit', 'quit', 'l'])
+    @checks.dj_only()
     async def leave(self, ctx):
         player = self.bot.lavalink.players.get(ctx.guild.id)
 
         if not player.is_connected:
-            return await ctx.send('🚫 Not connected.')
+            return await ctx.send('🚫 | Not connected.')
 
         if not ctx.author.voice or (player.is_connected and ctx.author.voice.channel.id != int(player.channel_id)):
-            return await ctx.send('🚫 You\'re not in my voice channel!')
+            return await ctx.send('🚫 | You\'re not in my voice channel!')
 
         if player.channel_id == '486765249488224277':
-            return await ctx.send('🚫 I have to stay in this channel!')
+            return await ctx.send('🚫 | I have to stay in this channel!')
 
         player.queue.clear()
-        if player.current is not None:
-            await Music.fade_out(player)
         await player.disconnect()
-        await ctx.send('*⃣ | Disconnected.')
-        await Music.fade_in(player)
+        return await ctx.send('*⃣ | Disconnected.')
