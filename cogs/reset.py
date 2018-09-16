13a7e6d14e960e174419f14bbf0e975bd7dabed2
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-16 13:10:56 +0200

    Imports, texts

--- cogs/reset.py
+++ cogs/reset.py
@@ -1,5 +1,7 @@
 from discord.ext import commands
 
+from utilities import checks
+
 
 def setup(bot):
     bot.add_cog(Reset(bot))
@@ -10,10 +12,11 @@ class Reset:
         self.bot = bot
 
     @commands.command(aliases=['restart', 'replay'])
+    @checks.dj_only()
     async def reset(self, ctx):
         player = self.bot.lavalink.players.get(ctx.guild.id)
         await player.seek(0)
-        await ctx.send('✅ Successfully reset the progress!')
+        await ctx.send('✅ | Successfully reset the progress!')
 
     async def get_player(self, context):
         out = self.bot.lavalink.players.get(context.guild.id)
