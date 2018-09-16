b57569f5e48749964e3c1df4bc14ac3b6c6550f0
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-16 13:08:56 +0200

    Imports, texts

--- cogs/move.py
+++ cogs/move.py
@@ -1,5 +1,7 @@
 from discord.ext import commands
 
+from utilities import checks
+
 
 def setup(bot):
     bot.add_cog(Move(bot))
@@ -10,6 +12,7 @@ class Move:
         self.bot = bot
 
     @commands.command(aliases=['mv'])
+    @checks.dj_only()
     async def move(self, ctx, *, move):
         player = self.bot.lavalink.players.get(ctx.guild.id)
 
@@ -19,17 +22,17 @@ class Move:
             move_song = int(moves[0])
             move_to = int(moves[1])
         except ValueError:
-            return await ctx.send('🚫 Please specify two valid positions!')
+            return await ctx.send('🚫 | Please specify two valid positions!')
 
         if move_song > len(player.queue) or move_song < 1:
-            return await ctx.send(f'🚫 {move_song} is not a valid!')
+            return await ctx.send(f'🚫 | {move_song} is not a valid position!')
 
         if move_to > len(player.queue) or move_to < 1:
-            return await ctx.send(f'🚫 {move_to} is not a valid!')
+            return await ctx.send(f'🚫 | {move_to} is not a valid position!')
 
         song = player.queue[move_song - 1]
         del player.queue[move_song - 1]
         player.queue.insert((move_to - 1), song)
 
-        await ctx.send(f'✅ Successfully moved **{song.title}** '
+        await ctx.send(f'✅ | Successfully moved **{song.title}** '
                        f'from position **`{move_song}`** to position **`{move_to}`**')
