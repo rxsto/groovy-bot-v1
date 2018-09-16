b2e98d6c011c02c963a24550e8d105d2a2bfdc87
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-16 13:09:56 +0200

    Texts

--- cogs/pause.py
+++ cogs/pause.py
@@ -14,10 +14,10 @@ class Pause:
         player = self.bot.lavalink.players.get(ctx.guild.id)
 
         if not player.is_playing:
-            return await ctx.send('🚫 I\'m not playing.')
+            return await ctx.send('🚫 | I\'m not playing.')
 
         if not player.paused:
             await player.set_pause(True)
             await ctx.send('⏸ | Paused')
         else:
-            await ctx.send('🚫 I\'m already paused!')
+            await ctx.send('🚫 | I\'m already paused!')
