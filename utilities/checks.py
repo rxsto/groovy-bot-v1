b6d55af6c50ec0352608207513fa5a154065917c
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-16 13:14:00 +0200

    Added checks

--- utilities/checks.py
+++ utilities/checks.py
@@ -1,5 +1,35 @@
 from discord.ext import commands
+
 from utilities import exceptions
+
+
+def dj_only():
+    async def predicate(ctx):
+        async with ctx.bot.get_postgre_client().get_pool().acquire() as connection:
+            statement = await connection.prepare('SELECT dj_mode FROM guilds WHERE id = $1')
+            dj_mode = await statement.fetchval(ctx.guild.id)
+            if dj_mode is False:
+                return True
+        if check_owner(ctx):
+            return True
+        if ctx.author.guild_permissions.administrator or ctx.author.guild_permissions.manage_guild:
+            return True
+        if "dj" in [y.name.lower() for y in ctx.author.roles]:
+            return True
+        raise exceptions.DjOnlyException
+
+    return commands.check(predicate)
+
+
+def admin_only():
+    async def predicate(ctx):
+        if check_owner(ctx):
+            return True
+        if ctx.author.guild_permissions.administrator or ctx.author.guild_permissions.manage_guild:
+            return True
+        raise exceptions.AdminOnlyException
+
+    return commands.check(predicate)
 
 
 def owner_only():
@@ -28,5 +58,3 @@ def premium_only(pledge):
             return True
 
     return commands.check(predicate)
-
-
