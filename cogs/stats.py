eed7183209d5591701c6d903879de0f0ece99db4
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-16 16:42:34 +0200

    Removed dumb package

--- cogs/stats.py
+++ cogs/stats.py
@@ -2,7 +2,6 @@ import datetime
 import time
 
 import discord
-import psutil
 from discord.ext import commands
 
 start_time = time.time()
@@ -26,8 +25,6 @@ class Stats:
         users = len(self.bot.users)
         voicechannels = len(self.bot.lavalink.players)
         latency = f'{int(self.bot.latency * 1000)}ms'
-        memory = f'{round(int(psutil.virtual_memory().used) / 1000000000, 2)}/' \
-                 f'{round(int(psutil.virtual_memory().total) / 1000000000, 2)} GB'
 
         embed = discord.Embed(
             description='📈 Groovys Statistics\n\n'
@@ -35,7 +32,6 @@ class Stats:
                         f'**Playing** {voicechannels}\n'
                         f'**Users** {users}\n'
                         f'**Latency** {latency}\n'
-                        f'**Memory** {memory}\n'
                         f'**Uptime** {uptime}\n',
             color=0x2C2F33
         ).set_thumbnail(url=ctx.me.avatar_url)
