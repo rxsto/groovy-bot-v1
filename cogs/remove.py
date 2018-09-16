f147ac10d001268b3b20f3bac6118e8a745c586d
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-16 13:10:49 +0200

    Imports, texts

--- cogs/remove.py
+++ cogs/remove.py
@@ -1,5 +1,7 @@
 from discord.ext import commands
 
+from utilities import checks
+
 
 def setup(bot):
     bot.add_cog(Temp(bot))
@@ -10,19 +12,20 @@ class Temp:
         self.bot = bot
 
     @commands.command(aliases=['rm'])
+    @checks.dj_only()
     async def remove(self, ctx, index: int):
         player = self.bot.lavalink.players.get(ctx.guild.id)
 
         if not index:
-            return await ctx.send('🚫 Please specify a position!')
+            return await ctx.send('🚫 | Please specify a position!')
 
         if not player.queue:
-            return await ctx.send('🚫 There\'s nothing queued!')
+            return await ctx.send('🚫 | There\'s nothing queued!')
 
         if index > len(player.queue) or index < 1:
-            return await ctx.send('🚫 Index has to be >=1 and <=queue size')
+            return await ctx.send('🚫 | Index has to be >=1 and <=queue size')
 
         index -= 1
         removed = player.queue.pop(index)
 
-        await ctx.send('Removed **' + removed.title + '** from the queue.')
+        await ctx.send('✅ | Removed **' + removed.title + '** from the queue.')
