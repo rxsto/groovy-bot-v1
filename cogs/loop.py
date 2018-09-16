7c5dd3611cfac97110e70e537feb185dda04fc06
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-16 13:08:35 +0200

    Imports, dj-only

--- cogs/loop.py
+++ cogs/loop.py
@@ -1,5 +1,7 @@
 from discord.ext import commands
 
+from utilities import checks
+
 
 def setup(bot):
     bot.add_cog(Loop(bot))
@@ -10,6 +12,7 @@ class Loop:
         self.bot = bot
 
     @commands.command(aliases=['lp'])
+    @checks.dj_only()
     async def loop(self, ctx):
         player = self.bot.lavalink.players.get(ctx.guild.id)
 
