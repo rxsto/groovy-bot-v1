1e69b9f41c66f64022940a63cd95b837602411de
Michael R.|SchlaumeierTV <mail@schlaubi.me>
2018-09-18 19:21:38 +0200

    Safe loading yaml

--- utilities/config.py
+++ utilities/config.py
@@ -4,7 +4,7 @@ import yaml
 class Config:
     def __init__(self):
         with open('config.yml', 'r') as f:
-            self.parsed_json = yaml.load(f)
+            self.parsed_json = yaml.safe_load(f)
 
     def get_config(self):
         return self.parsed_json
